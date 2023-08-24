daf1b842c0f048c13be26a021493b80b90d26571
tanveer-insta-web-works <101490981+tanveer-insta-web-works@users.noreply.github.com>
2023-08-24 14:18:16 +0600

    package updated

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const playwright = require("playwright-core");
 require("dotenv").config();
 
 let chrome = {};
-let puppeteer;
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  puppeteer = require("puppeteer");
-}
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
